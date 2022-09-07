a7aa9d84ddb31012d8fb6b5a290b36dbaeba72c6
draksitobikeado <103902831+Drakcito@users.noreply.github.com>
2022-09-06 23:02:01 -0500

    Add files via upload

---
+++ Ejercicio 1 PC1-u202213076.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main() {
+	long long code;
+	int merca_id, merca_ved, hora, minuto, segundo;
+
+	cout << "Ingrese el codigo de barras: ";//NN PP AA HH MM SS
+	cin >> code;//HH MM SS TTTTT E
+
+	hora = (code / 10000000000);
+	code = (code % 10000000000);
+
+	minuto = (code / 100000000);
+	code = (code % 100000000);
+
+	segundo = (code / 1000000);
+	code = (code % 1000000);
+
+	merca_id = (code / 10);
+	merca_ved = code % 10;
+
+	cout << "Hora de venta: " << hora << endl;
+	cout << "Minuto de venta: " << minuto << endl;
+	cout << "Segundos de venta: " << segundo << endl;
+	cout << "Mercancia de identificacion: " << merca_id << endl;
+	cout << "Mercancía vendida (1 por E-commerce; 0, no venta por E-commerce): " << merca_ved << endl;
+
+	return 0;
+}
