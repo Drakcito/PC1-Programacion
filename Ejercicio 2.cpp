d58eb677339db0c219732302237e49b4fc3bd31d
draksito0 <103902831+Drakcito@users.noreply.github.com>
2022-09-14 23:49:51 -0500

    Update Ejercicio 2.cpp

--- Ejercicio 2.cpp
+++ Ejercicio 2.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 using namespace std;
 
-float incremento(float precio, char juego) {
-	float incremento=0;
-	(juego == 'M') && (incremento = 0.25 * precio);
-	(juego == 'F') && (incremento = 0 * precio);
-	(juego == 'C') && (incremento = 0.1 * precio);
-	return incremento;
+float calculo_precio(float pmodelo, char juego) {
+	float precio;
+	((modelo = 5) && (color == 'B')) && (precio = 300.00);
+	((modelo = 5) && (color == 'M')) && (precio = 360.50);
+	((modelo = 5) && (color == 'O')) && (precio = 330.00);
+	((modelo = 4) && (color == 'B')) && (precio = 240.50);
+	((modelo = 4) && (color == 'M')) && (precio = 330.00);
+	((modelo = 4) && (color == 'O')) && (precio = 270.50);
+	return precio;
 }
-float calculo_precio(float precio,char juego) {
-	return (incremento(precio, juego) + precio);
+float calculo_incremento(char pjuego) {
+	float incremento;
+	(pjuego=='M')&&(incremento=0.25);
+	(pjuego=='F')&&(incremento=0);
+	(pjuego=='C')&&(incremento=0.1);
+	return incremento
 }
 
 int main() {
 	int modelo;
-	float precio;
+	float precio, incremento_precio, precio_final;
 	char juego, color;
 	cout << "Ingrese el modelo: ";
 	cin >> modelo;
@@ -24,15 +31,12 @@ int main() {
 	cin >> juego;
 	juego = toupper(juego);
 	color = toupper(color);
-
-	(modelo = 5) && (color == 'B') && (precio = 300.00);
-	(modelo = 5) && (color == 'M') && (precio = 360.50);
-	(modelo = 5) && (color == 'O') && (precio = 330.00);
-	(modelo = 4) && (color == 'B') && (precio = 240.50);
-	(modelo = 4) && (color == 'M') && (precio = 330.00);
-	(modelo = 4) && (color == 'O') && (precio = 270.50);
 	
-	cout << "Precio del equipo en $: " << calculo_precio(precio, juego);
+	precio= calculo_precio(modelo,color);
+	incremento_precio=calculo_incrememento(juego)*precio;
+	precio_final=precio+incremento_precio;
+	
+	cout << "Precio del equipo en $: " << precio_final;
 
 	return 0;
 }
